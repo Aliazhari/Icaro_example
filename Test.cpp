5de4ec3a0563743892c444f437fc30c07cc7204a
Ali Azhari <aliazhari@gmail.com>
2024-07-25 14:37:02 -0400

    My first commit of file Iraco tutorial

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+/******************************************
+*  Author : Ali Azhari   
+*  Created On : Thu Jul 25 2024
+*  File : Test.cpp
+*  Description: 
+*******************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+cout << "Hello World!" << endl;
+
+return 0;
+
+}
